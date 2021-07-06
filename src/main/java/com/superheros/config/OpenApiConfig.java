11fc212b754c665f5e6b10b8bd7f6da14defda9e
FelipeFernandez <f.fernandez@kubikdata.com>
2021-07-06 10:23:06 +0200

    feat: open api configuration

---
+++ src/main/java/com/superheros/config/OpenApiConfig.java
@@ -0,0 +1,25 @@
+package com.superheros.config;
+
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Super hero API")
+                        .description("Spring super hero application")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Super hero project")
+                        .url("https://github.com/felipefcor/super-heros"));
+    }
+}
